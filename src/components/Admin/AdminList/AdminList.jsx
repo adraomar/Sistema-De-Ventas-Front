7f1d59af0c869ca5e5bc990955cf27fad39aa9d1
Omar Adra <adra.omar2@gmail.com>
2026-05-19 20:01:05 -0300

    Modificado vista de productos ahora trae desde el backend

--- src/components/Admin/AdminList/AdminList.jsx
+++ src/components/Admin/AdminList/AdminList.jsx
@@ -1,61 +1,44 @@
 import React, { useEffect, useState } from "react";
-
 import api from "../../../api/api.js";
-
 import DataTable from "../../DataTable/DataTable";
 
 const AdminList = () => {
 
     const [users, setUsers] = useState([]);
-
     const [search, setSearch] = useState("");
 
     useEffect(() => {
-
         fetchUsers();
-
     }, []);
 
     const fetchUsers = async () => {
 
         try {
-
             const response = await api.get("/users/");
-
             setUsers(response.data);
-
         } catch (error) {
-
             console.error(error);
         }
     };
 
     /* EDIT */
-
     const handleEdit = (user) => {
-
         console.log("Editar:", user);
     };
 
     /* DELETE */
 
     const handleDelete = async (user) => {
-
         const confirmDelete =
             window.confirm(
                 `¿Eliminar usuario ${user.username}?`
             );
-
         if (!confirmDelete) return;
 
         try {
-
             await api.delete(`/users/${user.id}`);
-
             fetchUsers();
-
         } catch (error) {
-
             console.error(error);
         }
     };
@@ -63,7 +46,6 @@ const AdminList = () => {
     /* CREATE */
 
     const handleCreate = () => {
-
         console.log("Nuevo usuario");
     };
 
@@ -72,18 +54,13 @@ const AdminList = () => {
     const formattedUsers = users.map((user) => ({
 
         id: user.id,
-
         name: `${user.lastname}, ${user.firstname}`,
-
         username: user.username,
-
         email: user.email,
-
         role: user.rol
     }));
 
     return (
-
         <DataTable
 
             title="Administrar Usuarios"
@@ -97,19 +74,12 @@ const AdminList = () => {
             ]}
 
             data={formattedUsers}
-
             search={search}
-
             setSearch={setSearch}
-
             onCreate={handleCreate}
-
             onEdit={handleEdit}
-
             onDelete={handleDelete}
-
         />
-
     )
 }
 
--- src/components/Products/ProductsList/ProductsList.jsx
+++ src/components/Products/ProductsList/ProductsList.jsx
@@ -1,97 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, use } from 'react'
+import api from "../../../api/api.js";
 import DataTable from '../../DataTable/DataTable'
 
 const ProductsList = () => {
-    const [products, setProducts] = useState([
-        { id: 1, name: 'Producto 1', price: 100, stock: 5000 },
-        { id: 2, name: 'Producto 2', price: 200, stock: 5000 },
-        { id: 3, name: 'Producto 3', price: 300, stock: 5000 },
-        { id: 4, name: 'Producto 4', price: 400, stock: 5000 },
-        { id: 5, name: 'Producto 5', price: 500, stock: 5000 },
-        { id: 6, name: 'Producto 6', price: 600, stock: 5000 },
-        { id: 7, name: 'Producto 7', price: 700, stock: 5000 },
-        { id: 8, name: 'Producto 8', price: 800, stock: 5000 },
-        { id: 9, name: 'Producto 9', price: 900, stock: 5000 },
-        { id: 10, name: 'Producto 10', price: 1000, stock: 5000 },
-        { id: 11, name: 'Producto 11', price: 1100, stock: 5000 },
-        { id: 12, name: 'Producto 12', price: 1200, stock: 5000 },
-        { id: 13, name: 'Producto 1', price: 100, stock: 5000 },
-        { id: 14, name: 'Producto 2', price: 200, stock: 5000 },
-        { id: 15, name: 'Producto 3', price: 300, stock: 5000 },
-        { id: 16, name: 'Producto 4', price: 400, stock: 5000 },
-        { id: 17, name: 'Producto 5', price: 500, stock: 5000 },
-        { id: 18, name: 'Producto 6', price: 600, stock: 5000 },
-        { id: 19, name: 'Producto 7', price: 700, stock: 5000 },
-        { id: 20, name: 'Producto 8', price: 800, stock: 5000 },
-        { id: 21, name: 'Producto 9', price: 900, stock: 5000 },
-        { id: 22, name: 'Producto 10', price: 1000, stock: 5000 },
-        { id: 23, name: 'Producto 11', price: 1100, stock: 5000 },
-        { id: 24, name: 'Producto 12', price: 1200, stock: 5000 }
-    ])
-
+    const [products, setProducts] = useState([])
     const [search, setSearch] = useState('')
-    const [showModal, setShowModal] = useState(false)
-    const [currentProduct, setCurrentProduct] = useState({
-        id: null,
-        name: '',
-        price: '',
-        stock: ''
-    })
 
-    const [currentPage, setCurrentPage] = useState(1)
-    const productsPerPage = 10
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        try {
+            const response = await api.get("/products/");
+            setProducts(response.data);
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
 
     const filteredProducts = products.filter(p =>
         p.name.toLowerCase().includes(search.toLowerCase())
     )
 
-    const handleSearch = (value) => {
-        setSearch(value)
-        setCurrentPage(1)
-    }
-
-    const indexOfLast = currentPage * productsPerPage
-    const indexOfFirst = indexOfLast - productsPerPage
-    const currentProducts = filteredProducts.slice(indexOfFirst, indexOfLast)
-
-    const totalPages = Math.ceil(filteredProducts.length / productsPerPage)
-
     const handleCreate = () => {
-        setCurrentProduct({ id: null, name: '', price: '', stock: '' })
-        setShowModal(true)
+        console.log("Crear Producto");
     }
 
     const handleEdit = (product) => {
-        setCurrentProduct(product)
-        setShowModal(true)
+        console.log("Editar Producto:", product);
     }
 
-    const handleSave = () => {
-        if (!currentProduct.name || !currentProduct.price || !currentProduct.stock) return
-
-        if (currentProduct.id) {
-            setProducts(products.map(p =>
-                p.id === currentProduct.id ? currentProduct : p
-            ))
-        } else {
-            const newProduct = {
-                ...currentProduct,
-                id: Date.now()
-            }
-            setProducts([...products, newProduct])
-        }
-
-        setShowModal(false)
+    const handleDelete = (product) => {
+        console.log("Eliminar Producto ID:", product.id);
     }
 
-    const handleDelete = (id) => {
-        setProducts(products.filter(p => p.id !== id))
-    }
+    /* DATA */
+
+    const formattedProducts = products.map((product) => ({
+
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        stock: product.stock
+    }));
 
     return (
         <DataTable title="Administrar Productos"
             columns={["ID", "Nombre", "Precio ($)", "Stock"]}
-            data={products}
+            data={formattedProducts}
             onEdit={handleEdit}
             onDelete={handleDelete}
             onCreate={handleCreate}
